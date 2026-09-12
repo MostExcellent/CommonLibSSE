dadde72c0c4766ffa8ca93072f3e4cd6d3f737c4
David Glover <MostExcellent@users.noreply.github.com>
2026-09-11 17:42:37 -0700

    Update BGSAnimationSequencer actions member  container to match game's memory layout for AE
    _sentinel confirmed at 0x10 on AE (1.6.1170), which matches BSTFixedHashMap. Unsure for 1.5.x, so guarded with SUPPORT_SKYRIM_AE

--- include/RE/B/BGSAnimationSequencer.h
+++ include/RE/B/BGSAnimationSequencer.h
@@ -13,7 +13,11 @@ namespace RE
 		// members
 		std::uint32_t                             numSequences;  // 00
 		std::uint32_t                             pad04;         // 04
+#ifndef SUPPORT_SKYRIM_AE
 		BSTHashMap<BSFixedString, BGSActionData*> actions;       // 08
+#else
+		BSTFixedHashMap<BSFixedString, BGSActionData*> actions;  // 08 - _sentinel is at 0x10 on AE but unsure for 1.5.x
+#endif
 	};
 	static_assert(sizeof(BGSAnimationSequencer) == 0x38);
 }
