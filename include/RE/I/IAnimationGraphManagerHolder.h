c0e5019689f0d9e177bf2c56c60c7ce3312445e5
David Glover <MostExcellent@users.noreply.github.com>
2026-09-14 15:24:10 -0700

    Identified IAnimationGraphManagerHolder::Unk_0E as ShouldUpdateAnimation

--- include/RE/I/IAnimationGraphManagerHolder.h
+++ include/RE/I/IAnimationGraphManagerHolder.h
@@ -36,7 +36,7 @@ namespace RE
 		virtual void                        PostCreateAnimationGraphManager(BSTSmartPointer<BSAnimationGraphManager>& a_animGraphMgr);                                           // 0B - { return; }
 		virtual void                        Unk_0C(void);                                                                                                                        // 0C - { return; }
 		virtual void                        PostChangeAnimationManager(const BSTSmartPointer<BShkbAnimationGraph>& a_arg1, const BSTSmartPointer<BShkbAnimationGraph>& a_arg2);  // 0D - { return; }
-		virtual void                        Unk_0E(void);                                                                                                                        // 0E - { return 1; }
+		virtual bool                        ShouldUpdateAnimation();                                                                                                             // 0E - { return 1; }
 		[[nodiscard]] virtual std::uint32_t GetGraphVariableCacheSize() const;                                                                                                   // 0F - { return 0; }
 		virtual bool                        GetGraphVariableImpl1(const BSFixedString& a_variableName, float& a_out) const;                                                      // 10
 		virtual bool                        GetGraphVariableImpl2(const BSFixedString& a_variableName, std::int32_t& a_out) const;                                               // 11
